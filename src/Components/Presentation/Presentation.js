1393d84bef6db5a60bd37662c3b7fb60a0967735
fjcarrozzino <fjcarrozzino@gmail.com>
2022-08-03 19:03:07 -0300

    add assets and presentation

---
+++ src/Components/Presentation/Presentation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Presentation.css'
+
+const Presentation = () => {
+  return (
+    <div className='presentation__container'>
+        <h1>Hi, I'm Francisco <br/>
+            a Full Stack Developer <br/>
+            based in Argentina.</h1>
+    </div>
+  )
+}
+
+export default Presentation
