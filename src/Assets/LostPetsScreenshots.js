79eb068e06f1e3515959d15db0f04c427586bd04
Francisco Carrozzino <fjcarrozzino@gmail.com>
2023-09-13 10:29:02 +0100

    perros perdidos

---
+++ src/Assets/LostPetsScreenshots.js
@@ -0,0 +1,8 @@
+import screenshot1 from './lostpets1.jpg'
+import screenshot2 from './lostpets2.jpg'
+import screenshot3 from './lostpets3.jpg'
+import screenshot4 from './lostpets4.jpg'
+import screenshot5 from './lostpets5.jpg'
+
+
+export const LostPetsScreenshots = [screenshot1, screenshot2, screenshot3, screenshot4, screenshot5]
--- src/Components/Presentation/Presentation.js
+++ src/Components/Presentation/Presentation.js
@@ -6,7 +6,7 @@ const Presentation = () => {
     <div className='presentation__container'>
         <h1>Hi, I'm Francisco <br/>
             a Full Stack Developer <br/>
-            based in Argentina.</h1>
+        </h1>
     </div>
   )
 }
